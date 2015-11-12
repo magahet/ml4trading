842d1b265c20f577d7eee21e08a48f4fa83a735b
Gar Mendiola <magahet@gmail.com>
2015-11-12 09:56:51 -0800

    blah

--- mc3_p1/plotlearned.py
+++ mc3_p1/plotlearned.py
@@ -9,13 +9,15 @@ import LinRegLearner as lrl
 import KNNLearner as knn
 
 
-def plot(data, knn, lnl):
+def plot(data, knn_df, lnl_df):
     fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
     data.plot(kind='scatter', x='x', y='y', ax=ax1)
-    knn.plot(kind='scatter', x='x', y='y', ax=ax2)
-    lnl.plot(kind='scatter', x='x', y='y', ax=ax3)
-    ax1.set_xlabel("x")
-    ax1.set_ylabel("y")
+    knn_df.plot(kind='scatter', x='x', y='y', ax=ax2)
+    lnl_df.plot(kind='scatter', x='x', y='y', ax=ax3)
+    ax1.set_title('Actual')
+    ax2.set_title('KNN')
+    ax3.set_title('LinReg')
+    plt.tight_layout()
     plt.show()
 
 
@@ -31,7 +33,12 @@ if __name__ == '__main__':
     # separate out training and testing data
     trainX = data[:train_rows, 0:-1]
     trainY = data[:train_rows, -1]
-    testX = data[:, 0:-1]
+    if len(sys.argv) > 2 and sys.argv[2] == 'all':
+        testX = data[:, 0:-1]
+        testY = data[:, -1]
+    else:
+        testX = data[train_rows:, 0:-1]
+        testY = data[train_rows:, -1]
 
     # create a learner and train it
     lrl_l = lrl.LinRegLearner()
@@ -41,5 +48,8 @@ if __name__ == '__main__':
     lrl_y = lrl_l.query(testX)  # get the predictions
     knn_y = knn_l.query(testX)  # get the predictions
 
-    data = pd.DataFrame(data, columns=['x', 'y'])
-    plt.show()
+    knn_df = pd.DataFrame({'x': testX.flatten(), 'y': knn_y})
+    lrl_df = pd.DataFrame({'x': testX.flatten(), 'y': lrl_y})
+
+    data = pd.DataFrame({'x': testX.flatten(), 'y': testY.flatten()})
+    plot(data, knn_df, lrl_df)
